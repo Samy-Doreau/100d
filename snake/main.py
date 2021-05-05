e0b378d7ebd97694bbc28a4675d06deb2bc2d54d
Samy Doreau <samy.doreau@avanade.com>
2021-05-05 21:28:26 +0100

    Snake OOP 1/2

--- snake/main.py
+++ snake/main.py
@@ -1,21 +1,18 @@
 from turtle import Screen, Turtle
-
+from snake import Snake
+import time
+snake = Snake()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
+screen.tracer(0)
 
 
-t1 = Turtle(shape='square')
-t1.color('white')
-
-
-t2 = Turtle(shape='square')
-t2.color('white')
-t2.setx(t1.xcor() - 20)
-
-t3 = Turtle(shape='square')
-t3.color('white')
-t3.setx(t2.xcor() - 20)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 
 screen.exitonclick()
