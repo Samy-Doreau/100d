b441d0b3079c69e429021a11796a60f5030f7ecd
Samy Doreau <samy.doreau@avanade.com>
2021-05-05 10:05:38 +0100

    Coffe machine - OOP complete

--- CM_OOP/main.py
+++ CM_OOP/main.py
@@ -7,8 +7,30 @@ def coffee_machine():
     my_coffee_machine = CoffeeMaker()
     my_money_machine = MoneyMachine()
     my_menu = Menu()
+    machine_off = False
+    while not machine_off:
+        options = my_menu.get_items()
+        choice = input(f"Which drink would you like ({options}) ? \n > ")
+        if choice == 'report':
+            my_coffee_machine.report()
+            my_money_machine.report()
+        elif choice == 'off':
+            machine_off = True
+        else:
+            selected_drink = my_menu.find_drink(choice)
+            if selected_drink is not None:
+                # Check if sufficient resources
+                is_resource_sufficient = my_coffee_machine.is_resource_sufficient(
+                    selected_drink)
+                if is_resource_sufficient:
+                    # Take payment
+                    is_paid_amount_sufficient = False
+                    while not is_paid_amount_sufficient:
+                        is_paid_amount_sufficient = my_money_machine.make_payment(
+                            selected_drink.cost)
 
-    my_coffee_machine.report()
+                    # Once enough money provided, and transaction registered, make coffee
+                    my_coffee_machine.make_coffee(selected_drink)
 
 
 coffee_machine()
